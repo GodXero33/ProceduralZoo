5a1e9fb13eee5fdaad25b480bc3ed4a00aafda3e
GodXero <shansathish33@gmail.com>
2024-11-25 00:46:10 +0530

    update

--- js/creature.js
+++ js/creature.js
@@ -3,7 +3,9 @@ class Creature {
 		this.joinGap = 10;
 		this.length = 20;
 		this.chain = Array.from({ length: this.length }, (_, i) => {
-			return { x: -i * this.joinGap, y: 0 };
+			const x = -i * this.joinGap;
+			const width = 20;
+			return { x, y: 0, angle: 0, left: { x, y: width / 2 }, right: { x, y: -width / 2 }, width };
 		});
 		this.controls = {
 			move: false,
@@ -11,23 +13,69 @@ class Creature {
 			turnRight: false
 		};
 		this.position = { x: 0, y: 0 };
-		this.speed = 3;
+		this.speed = 0;
 		this.rotation = 0;
 		this.rotationFact = 0.05;
-		this.minimumRotationAngle = Math.PI * 0.05;
+		// this.minimumRotationAngle = Math.PI * 0.05;
+		this.maxSpeed = 5;
 	}
 
 	draw (ctx) {
+		const joinSize = 5;
+		ctx.lineWidth = 2;
 		ctx.fillStyle = '#fff';
+		ctx.strokeStyle = '#f55';
 
+		ctx.beginPath();
+		ctx.lineTo(this.chain[0].x, this.chain[0].y);
+
+		for (let i = 1; i < this.chain.length; i++) {
+			ctx.lineTo(this.chain[i].x, this.chain[i].y);
+		}
+
+		ctx.stroke();
+
+		/* this.chain.forEach(join => {
+			ctx.fillRect(join.x - joinSize / 2, join.y - joinSize / 2, joinSize, joinSize);
+
+			ctx.translate(join.x, join.y);
+			ctx.rotate(join.angle);
+			ctx.beginPath();
+			ctx.moveTo(0, 20);
+			ctx.lineTo(0, -20);
+			ctx.stroke();
+			ctx.rotate(-join.angle);
+			ctx.translate(-join.x, -join.y);
+		}); */
 		this.chain.forEach(join => {
-			ctx.fillRect(join.x - 5, join.y - 5, 10, 10);
+			ctx.fillRect(join.x - joinSize / 2, join.y - joinSize / 2, joinSize, joinSize);
+
+			ctx.beginPath();
+			ctx.moveTo(join.left.x, join.left.y);
+			ctx.lineTo(join.right.x, join.right.y);
+			ctx.stroke();
+		});
+	}
+
+	calculateBody() {
+		this.chain.forEach(join => {
+			const angle = join.angle;
+			const x = join.x;
+			const y = join.y;
+			const width = join.width / 2;
+
+			join.left.x = x - width * Math.sin(angle);
+			join.left.y = y + width * Math.cos(angle);
+
+			join.right.x = x + width * Math.sin(angle);
+			join.right.y = y - width * Math.cos(angle);
 		});
 	}
 
 	updateChain () {
 		this.chain[0].x = this.position.x;
 		this.chain[0].y = this.position.y;
+		this.chain[0].angle = this.rotation;
 
 		for (let i = 1; i < this.chain.length; i++) {
 			const dx = this.chain[i].x - this.chain[i - 1].x;
@@ -50,13 +98,20 @@ class Creature {
 
 				this.chain[i].x = this.chain[i - 1].x + Math.cos(angle) * this.joinGap;
 				this.chain[i].y = this.chain[i - 1].y + Math.sin(angle) * this.joinGap;
+				this.chain[i].angle = angle;
 			}
 		}
 	}
 
 	movement () {
 		if (this.controls.move) {
-			
+			this.speed += 0.1;
+
+			if (this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+		} else {
+			this.speed -= 0.1;
+
+			if (this.speed < 0) this.speed = 0;
 		}
 
 		const cos = Math.cos(this.rotation);
@@ -66,16 +121,17 @@ class Creature {
 		this.position.y += sin * this.speed;
 
 		if (this.controls.turnLeft) {
-			this.rotation -= this.rotationFact;
+			this.rotation -= this.rotationFact * Math.min(this.speed, 1);
 		}
 
 		if (this.controls.turnRight) {
-			this.rotation += this.rotationFact;
+			this.rotation += this.rotationFact * Math.min(this.speed, 1);
 		}
 	}
 
 	update () {
 		this.movement();
 		this.updateChain();
+		this.calculateBody();
 	}
 }
